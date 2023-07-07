75e9659368bfb0a9c2e20c9bd4b642cea62b1d6c
Hui Lap Man <huilapman@gmail.com>
2023-07-07 12:49:49 +0800

    Update 0013_roman-to-integer.cpp

--- 0013_roman-to-integer.cpp
+++ 0013_roman-to-integer.cpp
@@ -4,40 +4,38 @@ public:
         int result = 0;
 
         for (int i=0; i< s.length(); ++i) {
-            switch (s[i]) {
-                case 'I': 
-                    result += 1; 
-                    break;
-                case 'V': 
-                    if (i> 0 && s[i-1] == 'I')
-                        result -= 2;
-                    result += 5; 
-                    break;
-                case 'X':
-                    if (i> 0 && s[i-1] == 'I')
-                        result -= 2;
-                    result += 10; 
-                    break;
-                case 'L': 
-                    if (i> 0 && s[i-1] == 'X')
-                        result -= 20;
-                    result += 50; 
-                    break;
-                case 'C':
-                    if (i> 0 && s[i-1] == 'X')
-                        result -= 20;
-                    result += 100; 
-                    break;
-                case 'D':
-                    if (i> 0 && s[i-1] == 'C')
-                        result -= 200;
-                    result += 500; 
-                    break;
-                case 'M': 
-                    if (i> 0 && s[i-1] == 'C')
-                        result -= 200;
-                    result += 1000; 
-                    break;
+            if (s[i] == 'I') {
+                result += 1; 
+            }
+            else if (s[i] == 'V') {
+                if (i> 0 && s[i-1] == 'I')
+                    result -= 2;
+                result += 5; 
+            }
+            else if (s[i] == 'X') {
+                if (i> 0 && s[i-1] == 'I')
+                    result -= 2;
+                result += 10; 
+            }
+            else if (s[i] == 'L') {
+                if (i> 0 && s[i-1] == 'X')
+                    result -= 20;
+                result += 50; 
+            }
+            else if (s[i] == 'C') {
+                if (i> 0 && s[i-1] == 'X')
+                    result -= 20;
+                result += 100; 
+            }
+            else if (s[i] == 'D') {
+                if (i> 0 && s[i-1] == 'C')
+                    result -= 200;
+                result += 500; 
+            }
+            else if (s[i] == 'M') {
+                if (i> 0 && s[i-1] == 'C')
+                    result -= 200;
+                result += 1000; 
             }
         }
         return result;
