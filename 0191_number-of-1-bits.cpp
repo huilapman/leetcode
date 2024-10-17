ea5b7a50823fa8964141bdf53821f260cc6f9713
Hui Lap Man <huilapman@gmail.com>
2024-10-18 03:36:43 +0800

    Create 0191_number-of-1-bits.cpp

---
+++ 0191_number-of-1-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int w = 0;
+        for (int i=0; i<32; ++i) {
+            if (1 & (n >> i)) {
+                ++w;
+            }
+        }
+        return w;
+    }
+};
